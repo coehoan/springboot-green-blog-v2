e6eb3220fa310f824dc134fd2869a98a32f93c3f
coehoan <coehoan@naver.com>
2022-03-22 16:58:11 +0900

    7. 회원가입 완료

--- src/main/java/site/metacoding/blogv2/service/UserService.java
+++ src/main/java/site/metacoding/blogv2/service/UserService.java
@@ -4,8 +4,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import site.metacoding.blogv2.domain.user.User;
 import site.metacoding.blogv2.domain.user.UserRepository;
 import site.metacoding.blogv2.web.api.dto.user.JoinDto;
+import site.metacoding.blogv2.web.api.dto.user.LoginDto;
 
 @RequiredArgsConstructor
 @Service
@@ -17,5 +19,10 @@ public class UserService {
     public void 회원가입(JoinDto joinDto) {
         // save하면 db에 insert하고 insert 결과를 다시 return 해줌
         userRepository.save(joinDto.toEntity());
+    }
+
+    public User 로그인(LoginDto loginDto) {
+        User userEntity = userRepository.mLogin(loginDto.getUsername(), loginDto.getPassword());
+        return userEntity;
     }
 }
