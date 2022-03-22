8a4bc466c9b73cf53392bee5e44ca418034b68fa
coehoan <coehoan@naver.com>
2022-03-22 12:48:27 +0900

    5. 회원가입 완료

--- src/main/java/site/metacoding/blogv2/service/UserService.java
+++ src/main/java/site/metacoding/blogv2/service/UserService.java
@@ -1,13 +1,21 @@
 package site.metacoding.blogv2.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.blogv2.domain.user.UserRepository;
+import site.metacoding.blogv2.web.api.dto.user.JoinDto;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+
+    @Transactional
+    public void 회원가입(JoinDto joinDto) {
+        // save하면 db에 insert하고 insert 결과를 다시 return 해줌
+        userRepository.save(joinDto.toEntity());
+    }
 }
--- src/main/java/site/metacoding/blogv2/web/UserController.java
+++ src/main/java/site/metacoding/blogv2/web/UserController.java
@@ -1,6 +1,7 @@
 package site.metacoding.blogv2.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.blogv2.service.UserService;
@@ -10,4 +11,14 @@ import site.metacoding.blogv2.service.UserService;
 public class UserController {
 
     private final UserService userService;
+
+    @GetMapping("/joinForm")
+    public String joinForm() {
+        return "user/joinForm";
+    }
+
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "user/loginForm";
+    }
 }
