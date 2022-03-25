d3e043f21a367baffab3a7348db930b690824ab0
coehoan <coehoan@naver.com>
2022-03-25 12:31:56 +0900

    10. 회원정보보기 완료 - throw 처리

--- src/main/java/site/metacoding/blogv2/web/UserController.java
+++ src/main/java/site/metacoding/blogv2/web/UserController.java
@@ -3,9 +3,12 @@ package site.metacoding.blogv2.web;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
+import site.metacoding.blogv2.domain.user.User;
 import site.metacoding.blogv2.service.UserService;
 
 @RequiredArgsConstructor
@@ -14,6 +17,13 @@ public class UserController {
 
     private final UserService userService;
     private final HttpSession session;
+
+    @GetMapping("/s/user/{id}")
+    public String userInfo(@PathVariable Integer id, Model model) {
+        User userEntity = userService.회원정보(id);
+        model.addAttribute("user", userEntity);
+        return "user/updateForm";
+    }
 
     // @GetMapping("/logout")
     // public String logout() {
